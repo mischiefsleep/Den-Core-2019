2c8b9a90dc87b24ef5c1bb951ea10f3fd10291e3
MIZRAHI Guilhem <guilhem.mizrahi@gmail.com>
2019-09-27 11:54:17 -0600

    SSH log scanner added

---
+++ Systems/sshLogScanner/ssh_scan.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+bin="/bin";
+log_file="/var/log/auth.log";
+alert_log="/var/log/ssh-log/ssh.scan";
+email="EMAIL@ADDRESS.COM";
+
+echo "Log since $(date +"%Y-%m-%d    %T" -d "1 hour ago")">$alert_log;
+
+hour=$($bin/date +"%s" -d "1 hour ago");
+
+mapfile -t result <<< $($bin/awk -v awkvar=$HOSTNAME 'BEGIN { FS=awkvar; OFS="|" } /ailed|ailure/ {print $1,$2}' $log_file);
+
+count=0;
+
+for i in "${result[@]}"
+do
+    IFS="|" read entry_time message <<< $i;
+    et=$($bin/date -d "$entry_time" +"%s");
+    if [ $et -ge $hour ]
+    then
+        ((count++));
+        echo "$entry_time | $message">>$alert_log;
+    fi
+done
+
+echo "$count failures detected";
+
+if [ $count -gt 5 ]
+then
+    echo "Send alert sms to number";
+    mail -s "SSH alert" $email <<< "There were $count failed attempts to connect to $HOSTNAME since $($bin/date +"%T")";
+    cp $alert_log $alert_log$hour;
+else
+    echo "";
+fi
